bfbffb0ff0064ca25832dc71b8af68b674b2bcbd
Kristabell Johnson <bell77iam@gmail.com>
2021-03-03 22:21:06 -0700

    routes and models

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+const Workouts = require("../models/");
+const { db } = require("../routes");
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts/range", ({ req, res) => {
+    db.dbWorkout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+module.exports = router;
